38369680f383de57df10c32cb1dd0f656a111719
albanrousset@gmail.com <albanrousset@gmail.com@b96353c1-4682-f96d-eeaa-9ab732420017>
2012-04-13 14:14:26 +0000

    On recommence car Rabbitsvn c'est de la daube !

---
+++ trunk/SCS/include/ia.h
@@ -0,0 +1,13 @@
+/*
+ **********************************************************
+ *
+ *  Programme : 	ia.h
+ *
+ *  ecrit par : 	Rousset Alban & Kawczak Clement
+ *
+ *  resume : 		entete gestion ia pour jeux foursight
+ *
+ *  date :  		05/03/12
+ *
+ ***********************************************************
+ */
